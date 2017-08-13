4476566a124ef3be0c46606cf6c2299e71806020
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2017-08-13 16:42:10 +0200

    A flavor of directives

--- app/javascript/flavor_of_directives/flavor_of_directives.vue
+++ app/javascript/flavor_of_directives/flavor_of_directives.vue
@@ -1,6 +1,33 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div>
+    <!-- <template> -->
+      <h1 v-if="!message">You must send a message for help!</h1>
+      <h2 v-else>You have sent a message!</h2>
+      <!-- <p>Dispatch a messenger immediately!</p> -->
+      <!-- <p>To nearby kingdom of Hearts!</p> -->
+    <!-- </template> -->
+    <textarea v-model="message"></textarea>
+    <button v-if="message">
+      Send word to allies for help!
+    </button>
+    <br/>
+    <hr/>
+    <br/>
+    <div v-if="gender == 'male' || gender == 'female'">
+      <h1>Hello,
+	<span v-show="gender == 'male'">Mister {{ name }}</span>
+	<span v-if="gender == 'female'">Miss {{name}}.</span>
+      </h1>
+    </div>
+    <h1 v-else>So you can't decide. Fine!</h1>
+    <label for="gender">Enter your gender:</label>
+    <input name="gender" type="text" v-model="gender"/>
+    <label for="name">Enter your name:</label>
+    <input name="name" type="text" v-model="name"/>
+    <br/>
+    <hr/>
+    <br/>
+    <pre>{{ $data }}</pre>
   </div>
 </template>
 
@@ -8,7 +35,9 @@
 export default {
   data: function () {
     return {
-      message: "A flavor of directives."
+      message: "Our king is dead! Send help!",
+      name: 'Universe',
+      gender: 'female'
     }
   }
 }
