77645e286b1922481e8fefb2cba46ba421d31fd7
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2017-08-14 12:24:05 +0200

    common chunks, and moving all the things to the header to properly use turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import 'calendar'
+import 'flavor_of_directives'
+import 'list_rendering'
+
 console.log('Hello World from Webpacker')
+
--- app/javascript/packs/flavor_of_directives.js
+++
@@ -1,1 +0,0 @@
-import 'flavor_of_directives'
